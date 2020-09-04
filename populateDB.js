3c3724705b23d5f48906c844d4cd3008856a832e
Daniel Corner <daniel@raventelemetry.com>
2020-09-04 18:01:31 -0400

    stream tweets fix

--- populateDB.js
+++ populateDB.js
@@ -3,36 +3,54 @@ const faunadb = require("faunadb");
 q = faunadb.query;
 require("dotenv").config();
 
-const client = new faunadb.Client({
-  secret: process.env.FAUNA_DB_KEY,
-});
-
 /** usage:
  * `node populateDB.js 300` - populate with 300 tweets
  */
 
-const numTweetsDefault = 100;
-let numTweets = 0;
-
-stream.on("tweet", (tweet) => {
-  createTweetInDB(tweet);
-  numTweets++;
-
-  const numTweetsFromArgs = process.argv[2];
-
-  if (numTweets >= (numTweetsFromArgs || numTweetsDefault)) {
-    console.log(`finished writing ${numTweets} tweets to DB`);
-    process.exit(0);
-  }
+const faunaClient = new faunadb.Client({
+  secret: process.env.FAUNA_DB_KEY,
 });
 
+deleteAllTweetsInDB().then((ret) => {
+  console.log("deleted all tweets");
+  streamTweetsIntoDB();
+});
+
+function streamTweetsIntoDB() {
+  const numTweetsDefault = 10;
+  const numTweetsFromArgs = process.argv[2];
+  let numTweets = 0;
+
+  stream.on("tweet", (tweet) => {
+    createTweetInDB(tweet).then((ret) => {
+      numTweets++;
+      console.log("🌟🚨: streamTweetsIntoDB -> numTweets", numTweets);
+    });
+
+    if (numTweets >= (numTweetsFromArgs || numTweetsDefault)) {
+      console.log(`finished writing ${numTweets} tweets to DB`);
+      process.exit(0);
+    }
+  });
+}
+
+// TODO: switch to creating several posts
+// https://docs.fauna.com/fauna/current/tutorials/crud?lang=javascript#create-several-posts
 function createTweetInDB(tweet) {
-  client
+  return faunaClient
     .query(
       q.Create(q.Collection("Tweet"), {
         data: tweet,
       })
     )
-    .then((ret) => console.log(ret))
-    .catch(console.log);
+    .catch(console.error);
+}
+
+function deleteAllTweetsInDB() {
+  return faunaClient.query(
+    q.Map(
+      q.Paginate(q.Documents(q.Collection("Tweet"))),
+      q.Lambda((x) => q.Delete(x))
+    )
+  );
 }
