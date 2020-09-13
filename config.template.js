bcedde321e509ed1474bed67d3174df246b59909
Daniel Corner <daniel@raventelemetry.com>
2020-09-13 13:17:43 -0400

    add config.template

---
+++ config.template.js
@@ -0,0 +1,10 @@
+module.exports = {
+  config: {
+    consumer_key: "",
+    consumer_secret: "",
+    // app_only_auth:true,
+    access_token: "",
+    access_token_secret: "",
+  },
+  rapidapi_key: "",
+};
