7e56b2ccd17e09e63130350e5e29eaf401f50abd
Daniel Corner <daniel@raventelemetry.com>
2020-09-14 14:09:44 -0400

    fix config

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
+
 module.exports = {
   config: {
     // api key
-    consumer_key: "V9ygdvJgstQpK2RDJ5ECNBZih",
+    consumer_key: process.env.TWITTER_API_KEY,
     // api key secret
-    consumer_secret: "mNVC0S4mco0NX0PWOoyIJYTe3lNSeGUa15Tna68itlchCm0DSx",
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     // app_only_auth:true,
-    access_token: "3166664337-6R93zjS3ESvq9vRYvMlopTzYtRH06OrBkIItGee",
-    access_token_secret: "QHuFU5YG806CLTfXkdubh7lDSZgRLOCWvnd0r8qKdtz3Q",
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   },
   // botometer key (not yet used)
-  rapidapi_key: "52a578c65amshd84c2c17a4a34e2p1c11d9jsnef00b770bb13",
+  rapidapi_key: process.env.BOTOMETER_RAPIDAPI_KEY,
 };
