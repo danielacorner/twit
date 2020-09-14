132147443261fd982642c67b3a3d16f0837384fb
Daniel Corner <daniel@raventelemetry.com>
2020-09-14 11:33:10 -0400

    move config to env

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  config: {
+    // api key
+    consumer_key: process.env.TWITTER_API_KEY,
+    // api key secret
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    // app_only_auth:true,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  },
+  // botometer key (not yet used)
+  rapidapi_key: process.env.BOTOMETER_RAPIDAPI_KEY,
+};
