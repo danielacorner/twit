e08604b375333ab7ef2655ce47041d4804e82a52
Daniel Corner <daniel@raventelemetry.com>
2020-09-17 23:00:07 -0400

    commenting out unused

--- streamTweets.js
+++ streamTweets.js
@@ -11,34 +11,37 @@ async function streamTweets({ numTweets, filterFn }) {
     const tweets = [];
 
     console.log("Streaming tweets 🐦");
+    try {
+      stream.on("tweet", (tweet) => {
+        // if the tweet isn't filtered out...
+        if (filterFn && !filterFn(tweet)) {
+          return;
+        }
 
-    stream.on("tweet", (tweet) => {
-      // if the tweet isn't filtered out...
-      if (filterFn && !filterFn(tweet)) {
-        return;
-      }
+        // increment the count
+        count++;
 
-      // increment the count
-      count++;
-
-      // console log every so often
-      if (count % (filterFn ? 1 : 10) === 0) {
+        // console log every so often
+        // if (count % (filterFn ? 1 : 10) === 0) {
         console.log(`tweets fetched: ${count}`);
-      }
+        // }
 
-      // ? if too slow, move this to a separate batch request
-      // generate sentiment analysis
-      const sentimentResult = sentiment.analyze(
-        (tweet.extended_tweet || tweet).full_text
-      );
+        // ? if too slow, move this to a separate batch request
+        // generate sentiment analysis
+        const sentimentResult = sentiment.analyze(
+          (tweet.extended_tweet || tweet).full_text
+        );
 
-      tweets.push({ ...tweet, sentimentResult });
+        tweets.push({ ...tweet, sentimentResult });
 
-      // stop eventually
-      if (count === numTweets) {
-        stream.stop();
-        resolve(tweets);
-      }
-    });
+        // stop eventually
+        if (count === numTweets) {
+          stream.stop();
+          resolve(tweets);
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
   });
 }
