e6fb27374f5a0c8730f0349ad93d22465c85025d
daniel.corner <danielcorner7@gmail.com>
2021-08-03 17:33:15 -0400

    cors tweaks

--- server.js
+++ server.js
@@ -68,15 +68,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
