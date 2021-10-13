6129d1c3dc70e9d2107f76d7d3b7ca9520945025
dcorn068 <daniel@raventelemetry.com>
2021-10-12 23:53:18 -0400

    working on generateBotScore with api v2

--- functions/generateBotScore.js
+++ functions/generateBotScore.js
@@ -2,153 +2,166 @@ const getTimeline = require("./getTimeline");
 const axios = require("axios").default;
 
 const EMPTY_USER = {
-  id: 0,
-  id_str: "",
-  name: "",
-  username: "",
-  screen_name: "",
-  public_metrics: {
-    followers_count: 0,
-    following_count: 0,
-    tweet_count: 0,
-    listed_count: 0,
-  },
-  isNotABot: null,
-  botScore: null,
-  pinned_tweet_id: "",
-  hiddenBotScore: null,
-  location: null | "",
-  url: null | "",
-  description: null | "",
-  translator_type: null,
-  protected: null,
-  verified: null,
-  followers_count: 0,
-  friends_count: 0,
-  listed_count: 0,
-  favourites_count: 0,
-  statuses_count: 0,
-  created_at: "",
-  utc_offset: null,
-  time_zone: null,
-  geo_enabled: null,
-  lang: null,
-  contributors_enabled: null,
-  is_translator: null,
-  profile_background_color: "",
-  profile_background_image_url: "",
-  profile_background_image_url_https: "",
-  profile_background_tile: null,
-  profile_link_color: "#555",
-  profile_sidebar_border_color: "#eaeaea",
-  profile_sidebar_fill_color: "#eaeaea",
-  profile_text_color: "#000",
-  profile_use_background_image: null,
-  profile_image_url: "",
-  profile_image_url_https: "",
-  profile_banner_url: "",
-  default_profile: null,
-  default_profile_image: null,
-  following: null,
-  follow_request_sent: null,
-  notifications: null,
+	id: 0,
+	id_str: "",
+	name: "",
+	username: "",
+	screen_name: "",
+	public_metrics: {
+		followers_count: 0,
+		following_count: 0,
+		tweet_count: 0,
+		listed_count: 0,
+	},
+	isNotABot: null,
+	botScore: null,
+	pinned_tweet_id: "",
+	hiddenBotScore: null,
+	location: null | "",
+	url: null | "",
+	description: null | "",
+	translator_type: null,
+	protected: null,
+	verified: null,
+	followers_count: 0,
+	friends_count: 0,
+	listed_count: 0,
+	favourites_count: 0,
+	statuses_count: 0,
+	created_at: "",
+	utc_offset: null,
+	time_zone: null,
+	geo_enabled: null,
+	lang: null,
+	contributors_enabled: null,
+	is_translator: null,
+	profile_background_color: "",
+	profile_background_image_url: "",
+	profile_background_image_url_https: "",
+	profile_background_tile: null,
+	profile_link_color: "#555",
+	profile_sidebar_border_color: "#eaeaea",
+	profile_sidebar_fill_color: "#eaeaea",
+	profile_text_color: "#000",
+	profile_use_background_image: null,
+	profile_image_url: "",
+	profile_image_url_https: "",
+	profile_banner_url: "",
+	default_profile: null,
+	default_profile_image: null,
+	following: null,
+	follow_request_sent: null,
+	notifications: null,
 };
 
 async function generateBotScore(tweetsByUser, res) {
-  return new Promise(async (resolve, reject) => {
-    const user = tweetsByUser[0].user;
-    console.log("🌟🚨 ~ returnnewPromise ~ user", user);
-    if (!user) {
-      console.log("🚨 ~ NO USER", user);
-      return;
-    }
-    // * fetch ~50? tweets for user, then pass them into timeline here
-    const TWEETS_TO_FETCH = 200;
-    const timeline = await getTimeline({
-      userId: user.id_str || String(user.id),
-      numTweets: TWEETS_TO_FETCH,
-      screenName: user.screen_name,
-    });
+	return new Promise(async (resolve, reject) => {
+		const user = tweetsByUser[0].user;
+		console.log("🌟🚨 ~ returnnewPromise ~ user", user);
 
-    if (timeline.length === 0 || !Array.isArray(timeline)) {
-      console.log("🤡 no timeline!", timeline.length);
-      if (!Array.isArray(timeline)) {
-        console.log(timeline);
-      }
-      return;
-    }
+		// * fetch ~50? tweets for user, then pass them into timeline here
+		const TWEETS_TO_FETCH = 200;
+		const timeline = await getTimeline({
+			user,
+			numTweets: TWEETS_TO_FETCH,
+			screenName: user.screen_name,
+		});
 
-    const options = {
-      method: "POST",
-      url: "https://botometer-pro.p.rapidapi.com/4/check_account",
-      headers: {
-        "content-type": "application/json",
-        "x-rapidapi-host": "botometer-pro.p.rapidapi.com",
-        "x-rapidapi-key": process.env.BOTOMETER_RAPIDAPI_KEY,
-      },
-      data: {
-        mentions: [],
-        timeline,
-        // api v2 user is missing some data like banner image url, botometer requires a full user objest
-        user: { ...EMPTY_USER, user },
-      },
-    };
+		if (timeline.length === 0 || !Array.isArray(timeline)) {
+			console.log("🤡 no timeline!", timeline.length);
+			if (!Array.isArray(timeline)) {
+				console.log(timeline);
+			}
+			return;
+		}
 
-    // https://botometer.osome.iu.edu/faq
-    // https://rapidapi.com/OSoMe/api/botometer-pro/details
-    // check usage at https://rapidapi.com/developer/dashboard
+		const options = {
+			method: "POST",
+			url: "https://botometer-pro.p.rapidapi.com/4/check_account",
+			headers: {
+				"content-type": "application/json",
+				"x-rapidapi-host": "botometer-pro.p.rapidapi.com",
+				"x-rapidapi-key": process.env.BOTOMETER_RAPIDAPI_KEY,
+			},
+			data: {
+				user: { ...EMPTY_USER, user },
+				mentions: [],
+				timeline,
+				// api v2 user is missing some data like banner image url, botometer requires a full user objest
+			},
+		};
+		console.log(
+			"🌟🚨 ~ file: generateBotScore.js ~ line 95 ~ returnnewPromise ~ timeline[0]",
+			timeline[0]
+		);
 
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log("🌟 ~ response", response.statusText, response.status);
-        const {
-          cap, // Complete Automation Probability (CAP) is the conditional probability that accounts with a score equal to or greater than this are automated; based on inferred language
-          // While bot scores are useful for visualization and behavior analysis, they don't provide enough information by themselves to make a judgement about an account. A more meaningful way to interpret a score is to ask: "What are the chances that an account with a bot score higher than this account is human, or automated?" To answer this question, the Botometer API provides the so-called CAP, defined as the probability, according to our models, that an account with this score or greater is controlled by software, i.e., is a bot. (For the statisticians, this conditional probability calculation uses Bayes' theorem to take into account an estimate of the overall prevalence of bots, so as to balance false positives with false negatives.)
+		// https://botometer.osome.iu.edu/faq
+		// https://rapidapi.com/OSoMe/api/botometer-pro/details
+		// check usage at https://rapidapi.com/developer/dashboard
 
-          display_scores,
-          raw_scores,
-        } = response.data;
-        console.log("🌟 ~ raw_scores", raw_scores);
-        const { english, universal } = raw_scores;
+		axios
+			.request(options)
+			.then(function (response) {
+				console.log("🌟 ~ response", response.statusText, response.status);
+				const {
+					cap, // Complete Automation Probability (CAP) is the conditional probability that accounts with a score equal to or greater than this are automated; based on inferred language
+					// While bot scores are useful for visualization and behavior analysis, they don't provide enough information by themselves to make a judgement about an account. A more meaningful way to interpret a score is to ask: "What are the chances that an account with a bot score higher than this account is human, or automated?" To answer this question, the Botometer API provides the so-called CAP, defined as the probability, according to our models, that an account with this score or greater is controlled by software, i.e., is a bot. (For the statisticians, this conditional probability calculation uses Bayes' theorem to take into account an estimate of the overall prevalence of bots, so as to balance false positives with false negatives.)
 
-        // * currently we'll only use english
+					display_scores,
+					raw_scores,
+				} = response.data;
+				console.log("🌟 ~ raw_scores", raw_scores);
+				const { english, universal } = raw_scores;
 
-        // Bot scores are displayed on a 0-to-5 scale with zero being most human-like and five being the most bot-like. A score in the middle of the scale is a signal that our classifier is uncertain about the classification.
+				// * currently we'll only use english
 
-        //       Bot types:
+				// Bot scores are displayed on a 0-to-5 scale with zero being most human-like and five being the most bot-like. A score in the middle of the scale is a signal that our classifier is uncertain about the classification.
 
-        // fake_follower: bots purchased to increase follower counts
-        // self_declared: bots from botwiki.org
-        // astroturf: manually labeled political bots and accounts involved in follow trains that systematically delete content
-        // spammer: accounts labeled as spambots from several datasets
-        // financial: bots that post using cashtags
-        // other: miscellaneous other bots obtained from manual annotation, user feedback, etc.
+				//       Bot types:
 
-        const {
-          astroturf,
-          fake_follower,
-          financial,
-          other,
-          overall,
-          self_declared,
-          spammer,
-        } = universal; // universal = language-independent
+				// fake_follower: bots purchased to increase follower counts
+				// self_declared: bots from botwiki.org
+				// astroturf: manually labeled political bots and accounts involved in follow trains that systematically delete content
+				// spammer: accounts labeled as spambots from several datasets
+				// financial: bots that post using cashtags
+				// other: miscellaneous other bots obtained from manual annotation, user feedback, etc.
 
-        resolve({
-          astroturf,
-          fake_follower,
-          financial,
-          other,
-          overall,
-          self_declared,
-          spammer,
-        });
-      })
-      .catch(function (error) {
-        console.log("🌟 ~ returnnewPromise ~ error", error);
-      });
-  });
+				const {
+					astroturf,
+					fake_follower,
+					financial,
+					other,
+					overall,
+					self_declared,
+					spammer,
+				} = universal; // universal = language-independent
+
+				resolve({
+					astroturf,
+					fake_follower,
+					financial,
+					other,
+					overall,
+					self_declared,
+					spammer,
+				});
+			})
+			.catch(function (error) {
+				console.log("🌟 ~ returnnewPromise ~ error", Object.keys(error));
+				console.log(
+					"🌟 ~ returnnewPromise ~ error.response",
+					Object.keys(error.response)
+				);
+				console.log({
+					status: error.response.status,
+					statusText: error.response.statusText,
+				});
+				console.log(
+					"🌟🌟🌟 ~ returnnewPromise ~ error.data",
+					error.response.data
+				);
+			});
+	});
 }
 
 module.exports = generateBotScore;
--- functions/getTimeline.js
+++ functions/getTimeline.js
@@ -3,99 +3,106 @@ const { T } = require("../utils");
 const uniqBy = require("lodash.uniqby");
 const needle = require("needle");
 const {
-  getTwitterApiUrlQueryStringForTimeline,
+	getTwitterApiUrlQueryStringForTimeline,
 } = require("./getTwitterApiUrlQueryString");
 const token = process.env.TWITTER_BEARER_TOKEN;
 
 const MAX_ATTEMPTS = 10;
 
 /** https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline */
-async function getTimeline({ userId, numTweets, screenName, filterFn, maxId }) {
-  if (!userId) {
-    console.log("🤡 no userId!");
-    return;
-  }
-  try {
-    console.log("fetching timeline tweets 🐦");
+async function getTimeline({ user, numTweets, screenName, filterFn, maxId }) {
+	const userId = user.id_str || String(user.id);
+	if (!userId) {
+		console.log("🤡 no userId!");
+		return;
+	}
+	try {
+		console.log("fetching timeline tweets 🐦 for userId ", userId);
 
-    // if filtering by mediaType, keep fetching until we get that many
+		// if filtering by mediaType, keep fetching until we get that many
 
-    // * current strategy fetches numTweets repeatedly
-    // * could also fetch *remaining* numTweets repeatedly for faster results with greater API usage & number of attempts
+		// * current strategy fetches numTweets repeatedly
+		// * could also fetch *remaining* numTweets repeatedly for faster results with greater API usage & number of attempts
 
-    let attempts = 0;
-    let fetchedTweets = [];
-    let max_id = maxId || null;
+		let attempts = 0;
+		let fetchedTweets = [];
+		let max_id = maxId || null;
 
-    while (fetchedTweets.length < numTweets && attempts < MAX_ATTEMPTS) {
-      attempts++;
+		while (fetchedTweets.length < numTweets && attempts < MAX_ATTEMPTS) {
+			attempts++;
 
-      // const queryParams = [
-      //   { key: "tweet.fields", val: tweetFields.join(",") },
-      //   { key: "expansions", val: tweetExpansions.join(",") },
-      //   { key: "user.fields", val: userFields.join(",") },
-      //   { key: "media.fields", val: mediaFields.join(",") },
-      //   { key: "place.fields", val: placeFields.join(",") },
-      // ];
-      // const queryString = `?${queryParams
-      //   .map(({ key, val }) => `${key}=${val}`)
-      //   .join("&")}`;
-      const queryString = getTwitterApiUrlQueryStringForTimeline();
-      const max_results = Math.min(5, Math.max(numTweets, 100));
-      const timelineUrl = `https://api.twitter.com/2/users/${userId}/tweets${queryString}&max_results=${max_results}`;
-      console.log("🌟🚨 ~ getTimeline ~ timelineUrl", timelineUrl);
+			// const queryParams = [
+			//   { key: "tweet.fields", val: tweetFields.join(",") },
+			//   { key: "expansions", val: tweetExpansions.join(",") },
+			//   { key: "user.fields", val: userFields.join(",") },
+			//   { key: "media.fields", val: mediaFields.join(",") },
+			//   { key: "place.fields", val: placeFields.join(",") },
+			// ];
+			// const queryString = `?${queryParams
+			//   .map(({ key, val }) => `${key}=${val}`)
+			//   .join("&")}`;
+			const queryString = getTwitterApiUrlQueryStringForTimeline();
+			const max_results = Math.min(5, Math.max(numTweets, 100));
+			const timelineUrl = `https://api.twitter.com/2/users/${userId}/tweets${queryString}&max_results=${max_results}`;
+			console.log("🌟🚨 ~ getTimeline ~ timelineUrl", timelineUrl);
 
-      const response = await needle("get", timelineUrl, {
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-      });
-      if (response.body.errors) {
-        console.log(
-          "🌟🚨 ~ getTimeline ~ response.title,.detail",
-          response.title,
-          response.detail
-        );
-        console.log("🌟🚨 ~ getTimeline ~ response.body", response.body);
-        return [];
-      } else {
-        console.log("🌟🚨 ~ getTimeline ~ response", Object.keys(response));
-        console.log("🌟🚨 ~ getTimeline ~ response.body", response.body);
-        // console.log("🌟🚨 ~ getTimeline ~ response", response);
-      }
+			const response = await needle("get", timelineUrl, {
+				headers: {
+					authorization: `Bearer ${token}`,
+				},
+			});
+			if (response.body.errors) {
+				console.log(
+					"🌟🚨 ~ getTimeline ~ response.title,.detail",
+					response.title,
+					response.detail
+				);
+				// console.log("🌟🚨 ~ getTimeline ~ response.body", response.body);
+				return [];
+			} else {
+				console.log("🌟🚨 ~ getTimeline ~ response", Object.keys(response));
+				console.log(
+					"🌟🚨 ~ getTimeline ~ response.body.data.length",
+					response.body.data.length
+				);
+				if (!response.body.data.length) {
+					console.log("🌟🚨 ~ getTimeline ~ response.body", response.body);
+				}
+				// console.log("🌟🚨 ~ getTimeline ~ response", response);
+			}
 
-      // const result = await T.get(`statuses/user_timeline`, {
-      //   ...(userId ? { user_id: userId } : {}),
-      //   ...(screenName ? { screen_name: screenName } : {}),
-      //   ...(max_id ? { max_id } : {}),
-      //   count: numTweets /*  - fetchedTweets.length */,
-      //   include_rts: true,
-      //   exclude_replies: false,
-      // });
+			// const result = await T.get(`statuses/user_timeline`, {
+			//   ...(userId ? { user_id: userId } : {}),
+			//   ...(screenName ? { screen_name: screenName } : {}),
+			//   ...(max_id ? { max_id } : {}),
+			//   count: numTweets /*  - fetchedTweets.length */,
+			//   include_rts: true,
+			//   exclude_replies: false,
+			// });
 
-      if (!response) {
-        return [];
-      }
+			if (!response) {
+				return [];
+			}
 
-      // on the next attempt, fetch tweets older than the oldest one we just fetched
-      // max_id = response.body.data.reduce(
-      //   (acc, cur) => Math.min(acc, Number(cur.id_str)),
-      //   Infinity
-      // );
+			// on the next attempt, fetch tweets older than the oldest one we just fetched
+			// max_id = response.body.data.reduce(
+			//   (acc, cur) => Math.min(acc, Number(cur.id_str)),
+			//   Infinity
+			// );
 
-      fetchedTweets = uniqBy(
-        [
-          ...fetchedTweets,
-          ...response.body.data.filter(filterFn ? filterFn : () => true),
-        ],
-        (t) => t.id_str
-      );
-      console.log("🌟: getTimeline -> fetchedTweets", fetchedTweets.length);
-    }
+			fetchedTweets = uniqBy(
+				[
+					...fetchedTweets,
+					...response.body.data.filter(filterFn ? filterFn : () => true),
+				],
+				(t) => t.id_str
+			);
+			console.log("🌟: getTimeline -> fetchedTweets", fetchedTweets.length);
+		}
 
-    return fetchedTweets.slice(0, numTweets);
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
+		return fetchedTweets.map((t) => ({ ...t, user })).slice(0, numTweets);
+	} catch (err) {
+		console.log(err);
+		return err;
+	}
 }
